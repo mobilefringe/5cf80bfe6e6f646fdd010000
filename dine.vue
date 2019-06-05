8578bbab3e0e73ec048659f05839751ec35d921d
stephanie@mobilefringe.com @ CodeCloud <stephanie@mobilefringe.com @ CodeCloud>
2019-06-05 16:24:40 -0400

    Alameda Landing 2.0 1.0  /dine.vue

--- dine.vue
+++ dine.vue
@@ -185,7 +185,7 @@
                 dropDownCats() {
                     var vm = this;
                     var dining_cat =  _.find(this.processedCategories, function(o) { return o.name == "Dining"});
-                    console.log(dining_ca);
+                    console.log(dining_cat);
                     var subcategories = [];
                     if (dining_cat !== null && dining_cat !== undefined) {
                        subcategories = vm.findSubcategoriesByParentID(dining_cat.id);
