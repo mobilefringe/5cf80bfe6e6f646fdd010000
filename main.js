cc101d03187a98634f7f6c402d4a11b2fdec7e76
stephanie@mobilefringe.com @ CodeCloud <stephanie@mobilefringe.com @ CodeCloud>
2019-06-05 15:37:44 -0400

    Alameda Landing 2.0 1.0  /main.js

--- main.js
+++ main.js
@@ -152,21 +152,21 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'vuex-router-syn
                         url: "https://www.facebook.com",
                         iconClass: "fab fa-facebook"
                     },
-                    {
-                        name: "Twitter",
-                        url: "https://twitter.com",
-                        iconClass: "fab fa-twitter"
-                    },
+                    // {
+                    //     name: "Twitter",
+                    //     url: "https://twitter.com",
+                    //     iconClass: "fab fa-twitter"
+                    // },
                     {
                         name: "Instagram",
                         url: "https://www.instagram.com",
                         iconClass: "fab fa-instagram"
                     },
-                    {
-                        name: "SnapChat",
-                        url: "https://www.snapchat.com",
-                        iconClass: "fab fa-snapchat-ghost"
-                    },
+                    // {
+                    //     name: "SnapChat",
+                    //     url: "https://www.snapchat.com",
+                    //     iconClass: "fab fa-snapchat-ghost"
+                    // },
                     // {
                     //     name: "YouTube",
                     //     url: "https://www.youtube.com/",
