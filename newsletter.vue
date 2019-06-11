5f0d2d092fc382be0a8194044b0ab6f0b5a58a45
stephanie@mobilefringe.com @ CodeCloud <stephanie@mobilefringe.com @ CodeCloud>
2019-06-11 13:05:29 -0400

    Alameda Landing 2.0 1.0  /newsletter.vue

--- newsletter.vue
+++ newsletter.vue
@@ -13,25 +13,25 @@
                         <div class="col-md-12">
                             <breadcrumb></breadcrumb>
                             <div v-if="pageContent" v-html="pageContent.body"></div>
-                            <form class="newsletter_form form-horizontal" action="//mobilefringe.createsend.com/t/d/s/vjklyu/" method="post" @submit.prevent="validateBeforeSubmit">
+                            <form class="newsletter_form form-horizontal" action="//mobilefringe.createsend.com/t/d/s/oljiud/" method="post" @submit.prevent="validateBeforeSubmit">
                                 <div class="row">
                                     <div class="col-sm-6" >
-                                        <label for="fieldfihudt" class="accessibility">First Name</label>
-                                        <input v-model="form_data.first_name" required class="margin_20 form-control" id="fieldfihudt" name="cm-f-fihudt" type="text" placeholder="First Name">
+                                        <label for="fieldwtuuiu" class="accessibility">First Name</label>
+                                        <input v-model="form_data.first_name" required class="margin_20 form-control" id="fieldwtuuiu" name="cm-f-wtuuiu" type="text" placeholder="First Name">
                                     </div>
                                     <div class="col-sm-6" >
-                                        <label for="fieldfihudi" class="accessibility">Last Name</label>
-                                        <input v-model="form_data.last_name" required class="margin_20 form-control" id="fieldfihudi" name="cm-f-fihudi" type="text" placeholder="Last Name">
+                                        <label for="fieldwtuudl" class="accessibility">Last Name</label>
+                                        <input v-model="form_data.last_name" required class="margin_20 form-control" id="fieldwtuudl" name="cm-f-wtuudl" type="text" placeholder="Last Name">
                                     </div>
                                 </div>
                                 <div class="row">
                                     <div class="col-sm-6">
                                         <label for="newsletter_email" class="accessibility">Email</label>
-                                        <input v-model="form_data.email" required class="margin_20 form-control" name="cm-vjklyu-vjklyu" type="email" placeholder="Email" id="newsletter_email">
+                                        <input v-model="form_data.email" required class="margin_20 form-control" name="cm-oljiud-oljiud" type="email" placeholder="Email" id="newsletter_email">
                                     </div>
                                     <div class="col-sm-6">
-                                        <label for="fieldfihudd" class="accessibility">Phone Number</label>
-                                        <input id="fieldfihudd" v-model="form_data.phone" required class="margin_20 form-control" name="cm-f-fihudd" type="text" placeholder="Phone Number" />
+                                        <label for="fieldwtuuik" class="accessibility">Phone Number</label>
+                                        <input id="fieldwtuuik" v-model="form_data.phone" required class="margin_20 form-control" name="cm-f-wtuuik" type="text" placeholder="Phone Number" />
                                     </div>
                                 </div>
                                 <div class="row">
