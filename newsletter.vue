91ab8b93158553c1665260861d4001a0debba492
stephanie@mobilefringe.com @ CodeCloud <stephanie@mobilefringe.com @ CodeCloud>
2019-06-11 12:46:20 -0400

    Alameda Landing 2.0 1.0  /newsletter.vue

--- newsletter.vue
+++ newsletter.vue
@@ -129,7 +129,34 @@
                     } catch (e) {
                         console.log("Error loading data: " + e.message);
                     }
-               
+                },
+                validateBeforeSubmit(form) {
+                    this.$validator.validateAll().then((result) => {
+                        if (result) {
+                            let errors = this.errors;
+                            
+                            if(errors.length > 0) {
+                                console.log("Error");
+                                this.formError = true;
+                            }
+                            else {
+                                form.preventDefault();
+                                console.log("No Error", form);
+                                var vm = this;
+                                $.getJSON(
+                                form.target.action + "?callback=?",
+                                $(form.target).serialize(),
+                                function (data) {
+                                    if (data.Status === 400) {
+                                       vm.formError = true;
+                                    } else { // 200
+                                        vm.formSuccess = true;
+                                    }
+                                });
+                                
+                            }
+                        }
+                    })
                 }
             }
         });
