02e2670bf18514332da327261af08488f900c731
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-06-14 15:14:42 -0400

    Alameda Landing 2.0 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -267,7 +267,12 @@
             methods: {
                 loadData: async function() {
                     try {
-                        let results = await Promise.all([this.$store.dispatch("getData", "banners"), this.$store.dispatch("getData","promotions"), this.$store.dispatch("getData", "events"), this.$store.dispatch('LOAD_PAGE_DATA', {url: "https://peninsula.mallmaverick.com/api/v4/peninsula/social.json"})]);
+                        let results = await Promise.all([
+                            this.$store.dispatch("getData", "banners"), 
+                            this.$store.dispatch("getData","promotions"), 
+                            this.$store.dispatch("getData", "events"), 
+                            this.$store.dispatch('LOAD_PAGE_DATA', {url: "https://alamedacrossing.mallmaverick.com/api/v4/alamedacrossing/social.json"})
+                        ]);
                         return results;
                     } catch(e) {
                         console.log("Error loading data: " + e.message);    
