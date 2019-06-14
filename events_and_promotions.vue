2cd7552db4c68e8099753c2fe55fb2bd3bcded06
stephanie@mobilefringe.com @ CodeCloud <stephanie@mobilefringe.com @ CodeCloud>
2019-06-14 13:38:20 -0400

    Alameda Landing 2.0 1.0  /events_and_promotions.vue

--- events_and_promotions.vue
+++ events_and_promotions.vue
@@ -158,11 +158,11 @@
                             }
 
                             if (value.store != null && value.store != undefined && _.includes(value.store.image_url, 'missing')) {
-                                value.store.image_url = "//codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/image/png/1529532187000/eventsplaceholder2@2x.png";
+                                value.store.image_url = "//codecloud.cdn.speedyrails.net/sites/5cf80bfe6e6f646fdd010000/image/png/1560532995376/alameda_placeholder_images.png";
                             }
                             
                             if (_.includes(value.image_url, 'missing')) {
-                                value.image_url = "//codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/image/png/1529532187000/eventsplaceholder2@2x.png";
+                                value.image_url = "//codecloud.cdn.speedyrails.net/sites/5cf80bfe6e6f646fdd010000/image/png/1560532995376/alameda_placeholder_images.png";
                             }
                             
                             value.description_short = _.truncate(value.description, { 'length': 250, 'separator': ' ' });
