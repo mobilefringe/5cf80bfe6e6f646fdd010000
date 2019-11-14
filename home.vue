3cb5d4efc230cc3bedcba13ee97b473aca53562d
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-14 15:23:07 -0500

    Alameda Landing 2.0 1.2  /home.vue

--- home.vue
+++ home.vue
@@ -106,7 +106,7 @@
     </div>
 </template>
 <script>
-    define(["Vue", "vuex", "vue!vue-slick", "moment", "moment-timezone", "vue-moment", "vue!welcome_msg"], function (Vue, Vuex, slick, moment, tz, VueMoment, welcomeMessage) {
+    define(["Vue", "vuex", "vue-meta", "vue!vue-slick", "moment", "moment-timezone", "vue-moment", "vue!welcome_msg"], function (Vue, Vuex, Meta, slick, moment, tz, VueMoment, welcomeMessage) {
         return Vue.component("home-component", {
             template: template, // the variable template will be injected
             data: function() {
@@ -179,6 +179,8 @@
                     });
                     
                     this.dataLoaded = true;  
+                    this.meta = this.findMetaDataByPath(this.$route.path);
+
                 });
             },
             computed: {
