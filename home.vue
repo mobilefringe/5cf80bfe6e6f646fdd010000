9c12d472b2c3b144c9ebf30d0a00c94445e67953
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-02-11 14:22:35 -0500

    Alameda Landing 2.0 1.2  /home.vue

--- home.vue
+++ home.vue
@@ -218,7 +218,7 @@
                         console.log(value)
                         
                         
-                        if (showOnWebDate == null) {
+                        if (value.start_date == null) {
                             banners.push(value)
                             console.log('banners', banners)
                         }
