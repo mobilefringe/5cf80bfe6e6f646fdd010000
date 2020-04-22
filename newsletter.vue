8fe438b77f968b7a9b2e1746f17434a1d66e61e3
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2020-04-22 12:40:21 -0400

    Alameda Landing 2.0 1.2  /newsletter.vue

--- newsletter.vue
+++ newsletter.vue
@@ -76,7 +76,7 @@
     </div>
 </template>
 <script>
-    define(["Vue", "vuex", "jquery", "vee-validate", "json!site.json", "campaign_monitor"], function(Vue, Vuex, $, VeeValidate, site, campaign_monitor) {
+    define(["Vue", "vuex", "jquery", "vee-validate", "json!site.json", "campaign_monitor"], function(Vue, Vuex, $, VeeValidate, site, campaignMonitor) {
         Vue.use(VeeValidate);
         return Vue.component("newsletter-component", {
             template: template, // the variable template will be injected
